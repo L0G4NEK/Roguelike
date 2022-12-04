012990dfe2e7155363b719c1967ba7095867d4a5
L0G4NEK <darekw132@gmail.com>
2022-12-04 16:16:28 +0100

    Rat AI

--- main.js
+++ main.js
@@ -8,24 +8,6 @@ kaboom({
 
 // Sprites
 
-loadSprite("warrior", "assets/warrior.png", {
-  sliceX: 21,
-  sliceY: 8.5,
-  anims: {
-    idle: {
-      from: 126,
-      to: 127,
-      speed: 1,
-      loop: true,
-    },
-    run: {
-      from: 128,
-      to: 133,
-      "speed": 10,
-      "loop": true
-    },
-  },
-})
 
 loadSprite("warrior", "assets/warrior.png", {
   sliceX: 21,
@@ -45,8 +27,8 @@ loadSprite("warrior", "assets/warrior.png", {
     },
     dead: {
       from: 8,
-      to: 13,
-      "speed": 3,
+      to: 11,
+      "speed": 10,
     },
   },
 })
@@ -70,7 +52,7 @@ loadSprite("rat", "assets/rat.png", {
     dead: {
       from: 12,
       to: 14,
-      "speed": 3,
+      "speed": 10,
     }
   },
 })
@@ -92,6 +74,7 @@ const rat =  add([
     solid(),
     scale(2),
     sprite("rat", {anim: "idle"}),
+    state('move'),
     "enemy"
 ])
 
@@ -116,9 +99,6 @@ function getRandomPosition(tileW = 16, tileH = 16) {
 // Spawn
 
 function spawn(number, enemy) {
-  for ( let i = 0; i < number; i++ ) {
-    return  enemy * number
-  }
 }
 
 spawn(6, rat)
@@ -133,43 +113,51 @@ player.onCollide("enemy", (enemy) => {
 })
 
 player.onDeath(() => {
-  player.play("dead")
-  wait(1, () => {
-    destroy(player)
-  })
+  player.play('dead')
+  wait(1, () => destroy(player))
 })
 
 // Movement
 
-onKeyDown("up", () => {
-  player.move(0, -120)
+  onKeyDown("up", () => {
+    player.move(0, -120)
+  })
+
+  onKeyDown("down", () => {
+    player.move(0, 120)
+  })
+
+  onKeyDown("left", () => {
+    player.move(-120, 0)
+    player.flipX(true)
+  })
+
+  onKeyDown("right", () => {
+    player.move(120, 0)
+    player.flipX(false)
+  })
+
+  onKeyPress(["left", "right", "up", "down"], () => {
+    player.play("run")
+  })
+
+  onKeyRelease(["left", "right", "up", "down"], () => {
+    if (
+        !isKeyDown("left")
+        && !isKeyDown("right")
+        && !isKeyDown("up")
+        && !isKeyDown("down")
+    ) {
+      player.play("idle")
+    }
+  })
+
+//Rat AI
+rat.onStateUpdate("move", () => {
+  if (!player.exists()) return
+  const dir = player.pos.sub(rat.pos).unit()
+  rat.move(dir.scale(180))
 })
 
-onKeyDown("down", () => {
-  player.move(0, 120)
-})
-
-onKeyDown("left", () => {
-  player.move(-120, 0)
-  player.flipX(true)
-})
-
-onKeyDown("right", () => {
-  player.move(120, 0)
-  player.flipX(false)
-})
-
-onKeyPress(["left", "right", "up", "down"], () => {
-  player.play("run")
-})
-
-onKeyRelease(["left", "right", "up", "down"], () => {
-  if (
-      !isKeyDown("left")
-      && !isKeyDown("right")
-      && !isKeyDown("up")
-      && !isKeyDown("down")
-  ) {
-    player.play("idle")
-  }
-})
+// Have to manually call enterState() to trigger the onStateEnter("move") event we defined above.
+rat.enterState("move")
