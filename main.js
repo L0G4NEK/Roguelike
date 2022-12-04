f773271129e242f0adb4e6a1e149d8501f10163f
L0G4NEK <darekw132@gmail.com>
2022-12-04 11:51:34 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import kaboom from 'kaboom';
+
+
+const { add, pos, sprite, area } = kaboom({
+    fullscreen: true,
+    clearColor: [0, 0, 0, 1]
+});
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import k from './kaboom'
-
-import Roguelike from './scenes/Roguelike'
-import GameOver from './scenes/GameOver'
-
-k.scene('roguelike', Roguelike)
-k.scene('game-over', GameOver)
-
-k.start('roguelike')
