b59327123ca5cf975c3d2b0a76cfe16550a7738f
L0G4NEK <darekw132@gmail.com>
2022-12-15 12:53:39 +0100

    moved to public

--- public/src/modal.js
+++ public/src/modal.js
@@ -1,6 +1,33 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+const logout = document.getElementById('logout');
+const mainMenu = document.getElementById('mainMenu');
+
+const auth = firebase.auth();
+
+const goToMainMenu = () => {
+    window.location.assign('./mainPage');
+}
+
+mainMenu.addEventListener('click', goToMainMenu);
+
+const logOut = () => {
+    auth.signOut()
+        .then(() => {
+            window.location.assign('./signin');
+        })
+}
+
+logout.addEventListener('click', logOut);
+
+auth.onAuthStateChanged(user => {
+    if(!user) {
+        window.location.assign('./signin');
+    }
+})
+
+//modal
 
 openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
