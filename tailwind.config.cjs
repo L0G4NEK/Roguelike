6989f3816703bf0490561c73675bd6b61eba19ce
L0G4NEK <darekw132@gmail.com>
2022-12-15 13:36:08 +0100

    moved to public

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,9 +8,9 @@ module.exports = {
   theme: {
     extend: {
         backgroundImage: {
-            'sword': "url('/src/assets/sword.png')",
-            'armor': "url('/src/assets/Armor.png')",
-            'ring': "url('/src/assets/Ring.png')",
+            'sword': "url('./src/assets/sword.png')",
+            'armor': "url('./src/assets/Armor.png')",
+            'ring': "url('./src/assets/Ring.png')",
             'bg':"url('/public/img/bg.png')"
         }
     },
